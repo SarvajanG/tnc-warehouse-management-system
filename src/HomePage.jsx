d75f38eb5169d0013de58c58fa61ffe899bfe20a
Sarvajan <sarvajan321@hotmail.com>
2025-04-11 15:17:12 -0700

    added title to homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,6 +5,7 @@ import CommonButton from "./CommonButton";
 import useAuthChecker from "./useAuthChecker";
 import BarcodeReaderIcon from "@mui/icons-material/BarcodeReader";
 import InventoryIcon from "@mui/icons-material/Inventory";
+import { Typography } from "@mui/material";
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -13,6 +14,15 @@ export default function HomePage() {
 
   return (
     <div className="home-container">
+      <Typography
+        className="scan-in-title"
+        color={"white"}
+        fontWeight={"bold"}
+        fontSize="clamp(1rem, 4vw + 1rem, 2.5rem)" // Adjust these values as needed
+        textAlign={"center"}
+      >
+        Select an Operation
+      </Typography>
       <div className="home-item-container">
         <CommonButton
           startIcon={<BarcodeReaderIcon sx={{ color: "#008fff" }} />}
@@ -29,7 +39,7 @@ export default function HomePage() {
           }}
         />
         <CommonButton
-          startIcon={<InventoryIcon sx={{ color: "purple" }}/>}
+          startIcon={<InventoryIcon sx={{ color: "purple" }} />}
           text="VIEW INVENTORY"
           onClick={() => {
             navigate("/inventory");
