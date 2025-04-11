756d6a1b4e999b67bcdeda473bee87743e6474b6
Sarvajan <sarvajan321@hotmail.com>
2025-04-11 02:34:44 -0700

    Refactored settings button and menu into its own component and used material UI features

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,15 +1,12 @@
 import "./HomePage.css";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import HighlightOffIcon from "@mui/icons-material/HighlightOff";
-import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { auth } from "./firebase";
-import { signOut, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import Settings from "./Settings";
 
 export default function HomePage() {
   const navigate = useNavigate();
-  const [settingsVisible, setSettingsVisible] = useState(false);
 
   // Check if user is already authenticated and redirect if necessary
   useEffect(() => {
@@ -25,20 +22,6 @@ export default function HomePage() {
     // Clean up the subscription on component unmount
     return unsubscribe;
   }, [navigate]);
-  
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);
-      navigate("/", { replace: true }); // Redirect to the authentication page
-      navigate(0); // Refresh the page to avoid history buttons being used
-    } catch (error) {
-      console.error("Error signing out: ", error);
-    }
-  };
-
-  const toggleSettings = () => {
-    setSettingsVisible(!settingsVisible);
-  };
 
   return (
     <div className="home-container">
@@ -68,30 +51,7 @@ export default function HomePage() {
           View Inventory
         </button>
       </div>
-      <div className="home-settings-button-container">
-        <ManageAccountsIcon
-          className="home-settings-button"
-          fontSize=""
-          onClick={toggleSettings}
-        />
-      </div>
-      {settingsVisible && (
-        <div className="home-settings">
-          <HighlightOffIcon
-            className="home-exit-settings-button"
-            fontSize=""
-            onClick={toggleSettings}
-          />
-          <Button
-            className="home-sign-out-button"
-            variant="contained"
-            sx={{ backgroundColor: "purple" }}
-            onClick={handleSignOut}
-          >
-            Sign Out
-          </Button>
-        </div>
-      )}
+      <Settings />
     </div>
   );
 }
---
+++ src/Settings.jsx
@@ -0,0 +1,75 @@
+import { Button, IconButton } from "@mui/material";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase";
+
+export default function Settings() {
+  const navigate = useNavigate();
+  const [settingsVisible, setSettingsVisible] = useState(false);
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/", { replace: true }); // Redirect to the authentication page
+      navigate(0); // Refresh the page to avoid history buttons being used
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+
+  const toggleSettings = () => {
+    setSettingsVisible(!settingsVisible);
+  };
+
+  return (
+    <div>
+      <IconButton
+        sx={{
+          height: "4rem",
+          width: "4rem",
+          position: "fixed",
+          bottom: "1rem",
+          right: "1rem",
+          color: "orange",
+          "&:hover": {
+            backgroundColor: "rgba(255, 165, 0, 0.1)", // Light orange background on hover
+          },
+        }}
+        onClick={toggleSettings}
+      >
+        <ManageAccountsIcon sx={{ fontSize: "3rem" }} />
+      </IconButton>
+
+      {settingsVisible && (
+        <div className="home-settings">
+          <IconButton
+            sx={{
+              height: "4rem",
+              width: "4rem",
+              position: "absolute",
+              top: "1rem",
+              right: "1rem",
+              color: "purple",
+              "&:hover": {
+                backgroundColor: "rgba(38, 0, 255, 0.27)", // Light purple background on hover
+              },
+            }}
+            onClick={toggleSettings}
+          >
+            <HighlightOffIcon sx={{ fontSize: "3rem" }} />
+          </IconButton>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "purple", width: "80%", height: "3rem" }}
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
